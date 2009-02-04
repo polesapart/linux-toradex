80f14f524ac37e4f1c1210dcb3941d243dcf56ce
Pedro Perez de Heredia <pperez@pperez-desktop.(none)>
2009-02-04 01:04:16 +0100

    Add support for S3C2443 ide interface
    The S3C2443 needs to ack the interrupt by writing into the ATA_IRQ
    register.

--- arch/arm/include/asm/ide.h
+++ arch/arm/include/asm/ide.h
@@ -18,6 +18,11 @@
 #define __ide_mm_outsw(port,addr,len)	writesw(port,addr,len)
 #define __ide_mm_outsl(port,addr,len)	writesl(port,addr,len)
 
+#ifdef CONFIG_CPU_S3C2443
+# define IDE_ARCH_ACK_INTR
+# define ide_ack_intr(hwif)	((hwif)->ack_intr ? (hwif)->ack_intr(hwif) : 1)
+#endif /* CONFIG_CPU_S3C2443 */
+
 #endif /* __KERNEL__ */
 
 #endif /* __ASMARM_IDE_H */
