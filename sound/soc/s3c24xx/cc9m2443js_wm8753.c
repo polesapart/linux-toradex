2a33b4016d4180cc154e09912d546e07acc27b43
Luis Galdos <luis.galdos@digi.com>
2009-03-02 11:00:26 +0100

    cc9m2443-snd: Disabled unused DAPM widgets
    The Neo-driver provides some widgets and streams
    which will not be available by the cc9m2443js. For
    this reason removed.
    
    Signed-off-by: Luis Galdos <luis.galdos@digi.com>

--- sound/soc/s3c24xx/cc9m2443js_wm8753.c
+++ sound/soc/s3c24xx/cc9m2443js_wm8753.c
@@ -49,23 +49,9 @@
 #include "s3c24xx-pcm.h"
 #include "s3c2412-i2s.h"
 
-/* define the scenarios */
-#define NEO_AUDIO_OFF			0
-#define NEO_GSM_CALL_AUDIO_HANDSET	1
-#define NEO_GSM_CALL_AUDIO_HEADSET	2
-#define NEO_GSM_CALL_AUDIO_BLUETOOTH	3
-#define NEO_STEREO_TO_SPEAKERS		4
-#define NEO_STEREO_TO_HEADPHONES	5
-#define NEO_CAPTURE_HANDSET		6
-#define NEO_CAPTURE_HEADSET		7
-#define NEO_CAPTURE_BLUETOOTH		8
-
-
 /* Additional info and error messages */
 #define printk_err(fmt, args...)                printk(KERN_ERR "[ ERROR ] cc9m2443-snd: " fmt, ## args)
 #define printk_info(fmt, args...)               printk(KERN_INFO "cc9m2443-snd: " fmt, ## args)
-
-
 
 #if 1
 #define CC9M2443JS_SND_DEBUG
@@ -76,7 +62,6 @@
 #else
 #  define printk_debug(fmt, args...)
 #endif
-
 
 static int cc9m2443js_hifi_hw_params(struct snd_pcm_substream *substream,
 	struct snd_pcm_hw_params *params)
@@ -126,7 +111,7 @@ static int cc9m2443js_hifi_hw_params(struct snd_pcm_substream *substream,
 	printk_debug("Sample rate %i | Master Clk %lu Hz | FS %u | Clk divisor %u\n",
 		     params_rate(params), clk_get_rate(s3c2412_get_iisclk()),
 		     info.fs_div, info.clk_div);
-	
+
 	/* REQUIRED: Set codec DAI configuration (normal bclk and frm ) and as slave */
 	ret = codec_dai->dai_ops.set_fmt(codec_dai,
 					 SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
@@ -208,7 +193,6 @@ static int cc9m2443js_hifi_hw_free(struct snd_pcm_substream *substream)
 	return codec_dai->dai_ops.set_pll(codec_dai, WM8753_PLL1, 0, 0);
 }
 
-
 static int cc9m2443js_hifi_startup(struct snd_pcm_substream *substream)
 {
 	printk_debug("Going to startup the codec\n");
@@ -219,10 +203,8 @@ static int cc9m2443js_hifi_startup(struct snd_pcm_substream *substream)
 /* We don't need this function, but we know that they are available */
 static void cc9m2443js_hifi_shutdown(struct snd_pcm_substream *substream)
 {
-
 	printk_debug("Going to shutdown the codec\n");
 }
-
 
 /*
  * WM8753 HiFi DAI operations.
@@ -232,167 +214,6 @@ static struct snd_soc_ops cc9m2443js_hifi_ops = {
 	.hw_free = cc9m2443js_hifi_hw_free,
 	.startup = cc9m2443js_hifi_startup,
 	.shutdown = cc9m2443js_hifi_shutdown,
-};
-
-static int cc9m2443js_voice_hw_params(struct snd_pcm_substream *substream,
-	struct snd_pcm_hw_params *params)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->dai->codec_dai;
-	unsigned int pcmdiv = 0;
-	int ret = 0;
-
-	printk_err("@XXX: Remove this function (no more required).\n");
-	
-	if (params_rate(params) != 8000)
-		return -EINVAL;
-	if (params_channels(params) != 1)
-		return -EINVAL;
-
-	pcmdiv = WM8753_PCM_DIV_6; /* 2.048 MHz */
-
-	/* todo: gg check mode (DSP_B) against CSR datasheet */
-	/* set codec DAI configuration */
-	ret = codec_dai->dai_ops.set_fmt(codec_dai, SND_SOC_DAIFMT_DSP_B |
-		SND_SOC_DAIFMT_NB_NF | SND_SOC_DAIFMT_CBS_CFS);
-	if (ret < 0)
-		return ret;
-
-	/* set the codec system clock for DAC and ADC */
-	ret = codec_dai->dai_ops.set_sysclk(codec_dai, WM8753_PCMCLK, 12288000,
-		SND_SOC_CLOCK_IN);
-	if (ret < 0)
-		return ret;
-
-	/* set codec PCM division for sample rate */
-	ret = codec_dai->dai_ops.set_clkdiv(codec_dai, WM8753_PCMDIV, pcmdiv);
-	if (ret < 0)
-		return ret;
-
-	return 0;
-}
-
-static int cc9m2443js_voice_hw_free(struct snd_pcm_substream *substream)
-{
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->dai->codec_dai;
-
-	printk_err("@XXX: Remove this function (no more required).\n");
-	
-	/* disable the PLL */
-	return codec_dai->dai_ops.set_pll(codec_dai, WM8753_PLL2, 0, 0);
-}
-
-
-static const struct snd_soc_dapm_widget wm8753_dapm_widgets[] = {
-	SND_SOC_DAPM_LINE("Audio Out", NULL),
-	SND_SOC_DAPM_LINE("GSM Line Out", NULL),
-	SND_SOC_DAPM_LINE("GSM Line In", NULL),
-	SND_SOC_DAPM_MIC("Headset Mic", NULL),
-	SND_SOC_DAPM_MIC("Call Mic", NULL),
-};
-
-
-
-static const struct snd_soc_dapm_route dapm_routes[] = {
-
-        /* Connections to the lm4857 amp */
-        {"Audio Out", NULL, "LOUT1"},
-        {"Audio Out", NULL, "ROUT1"},
-
-        /* Connections to Headset */
-        {"MIC1", NULL, "Mic Bias"},
-        {"Mic Bias", NULL, "Headset Mic"},
-
-        /* Call Mic */
-        {"MIC2", NULL, "Mic Bias"},
-        {"MIC2N", NULL, "Mic Bias"},
-        {"Mic Bias", NULL, "Call Mic"},
-
-        /* Connect the ALC pins */
-        {"ACIN", NULL, "ACOP"},
-};
-
-
-static const char *neo_scenarios[] = {
-	"Off",
-	"Speakers",
-	"Headphones",
-/* 	"Capture Handset", */
-/* 	"Capture Headset", */
-};
-
-static const struct soc_enum neo_scenario_enum[] = {
-	SOC_ENUM_SINGLE_EXT(ARRAY_SIZE(neo_scenarios), neo_scenarios),
-};
-
-static const struct snd_kcontrol_new wm8753_cc9m2443js_controls[] = {
-/* 	SOC_SINGLE_EXT("Amp Left Playback Volume", LM4857_LVOL, 0, 31, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-/* 	SOC_SINGLE_EXT("Amp Right Playback Volume", LM4857_RVOL, 0, 31, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-/* 	SOC_SINGLE_EXT("Amp Mono Playback Volume", LM4857_MVOL, 0, 31, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-/* 	SOC_ENUM_EXT("Amp Mode", lm4857_mode_enum[0], */
-/* 		lm4857_get_mode, lm4857_set_mode), */
-/* 	SOC_ENUM_EXT("Neo Mode", neo_scenario_enum[0], */
-/* 		cc9m2443js_get_scenario, cc9m2443js_set_scenario), */
-/* 	SOC_SINGLE_EXT("Amp Spk 3D Playback Switch", LM4857_LVOL, 5, 1, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-/* 	SOC_SINGLE_EXT("Amp HP 3d Playback Switch", LM4857_RVOL, 5, 1, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-/* 	SOC_SINGLE_EXT("Amp Fast Wakeup Playback Switch", LM4857_CTRL, 5, 1, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-/* 	SOC_SINGLE_EXT("Amp Earpiece 6dB Playback Switch", LM4857_CTRL, 4, 1, 0, */
-/* 		lm4857_get_reg, lm4857_set_reg), */
-};
-
-/*
- * This is an example machine initialisation for a wm8753 connected to a
- * neo1973 II. It is missing logic to detect hp/mic insertions and logic
- * to re-route the audio in such an event.
- */
-static int cc9m2443js_wm8753_snd_init(struct snd_soc_codec *codec)
-{
-	int i, err;
-
-	/* set up NC codec pins */
-	snd_soc_dapm_disable_pin(codec, "LOUT2");
-	snd_soc_dapm_disable_pin(codec, "ROUT2");
-	snd_soc_dapm_disable_pin(codec, "OUT3");
-	snd_soc_dapm_disable_pin(codec, "OUT4");
-	snd_soc_dapm_disable_pin(codec, "LINE1");
-	snd_soc_dapm_disable_pin(codec, "LINE2");
-
-	/* set endpoints to default mode */
-	/* set_scenario_endpoints(codec, NEO_AUDIO_OFF); */
-
-	/* Add neo1973 specific widgets */
-	for (i = 0; i < ARRAY_SIZE(wm8753_dapm_widgets); i++)
-		snd_soc_dapm_new_control(codec, &wm8753_dapm_widgets[i]);
-
-	/* Add specific controls */
-	for (i = 0; i < ARRAY_SIZE(wm8753_cc9m2443js_controls); i++) {
-		err = snd_ctl_add(codec->card,
-				snd_soc_cnew(&wm8753_cc9m2443js_controls[i],
-				codec, NULL));
-		if (err < 0)
-			return err;
-	}
-
-	/* set up specific audio path audio_mapnects */
-	err = snd_soc_dapm_add_routes(codec, dapm_routes, ARRAY_SIZE(dapm_routes));
-	if (err)
-		printk_err("Couldn't add the routes (%i)\n", err);
-	else
-		snd_soc_dapm_sync(codec);
-	
-	return err;
-}
-
-static struct snd_soc_ops cc9m2443js_voice_ops = {
-	.hw_params = cc9m2443js_voice_hw_params,
-	.hw_free = cc9m2443js_voice_hw_free,
 };
 
 /*
@@ -409,18 +230,18 @@ static struct snd_soc_dai_link cc9m2443js_dai[] = {
 		.stream_name = "WM8753 HiFi",
 		.cpu_dai = &s3c2443_i2s_dai,
 		.codec_dai = &wm8753_dai[WM8753_DAI_HIFI],
-		.init = cc9m2443js_wm8753_snd_init,
+		/* .init = cc9m2443js_wm8753_snd_init, */
 		.ops = &cc9m2443js_hifi_ops,
 	},{
 		/*
 		 * Voice
 		 * Infos under: proc/asound/card0/pcm1c/info
 		 */
-		.name = "Voice",
-		.stream_name = "WM8753 Voice",
+		.name = "Inline",
+		.stream_name = "WM8753 Inline",
 		.cpu_dai = &s3c2443_i2s_dai,
-		.codec_dai = &wm8753_dai[WM8753_DAI_VOICE],
-		.ops = &cc9m2443js_voice_ops,
+		.codec_dai = &wm8753_dai[WM8753_DAI_HIFI],
+		.ops = &cc9m2443js_hifi_ops,
 	}
 };
 
