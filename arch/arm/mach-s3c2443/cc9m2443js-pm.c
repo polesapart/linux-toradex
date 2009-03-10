da4f2f9e28c66256eae5fbbd57fa219340b80897
Luis Galdos <luis.galdos@digi.com>
2009-03-10 23:54:41 +0100

    cc9m2443-pm: Added the wakeup-GPIOs
    For having an easier handling of the wakeup-GPIOs,
    the code includes a small driver for registering
    and configuring the GPIO/IRQ. The wakeup can be
    enabled over the sysfs-attribute:
    
        /sys/devices/platform/cc9m2443-wio.232/power/wakeup
    
    The above attribute enables/disables the wakeup
    of the GPIO number 232.
    
    The included GPIOs are 226 and 232. They can be
    selected over:
    
        System type > S3C2443 Machines > Enable GPIO ...
    
    Signed-off-by: Luis Galdos <luis.galdos@digi.com>

--- arch/arm/mach-s3c2443/cc9m2443js-pm.c
+++ arch/arm/mach-s3c2443/cc9m2443js-pm.c
@@ -17,6 +17,7 @@
 #include <linux/ioport.h>
 #include <linux/delay.h>
 #include <linux/io.h>
+#include <linux/platform_device.h>
 #include <mach/memory.h>
 #include <asm/memory.h>
 #include <mach/map.h>
@@ -39,6 +40,14 @@
 #define CC9M2443_PM_ITEM_OFFSET(off)           { .offset = off }
 #define CC9M2443_PM_ITEM_BASE(bas)             { .base = bas }
 
+struct cc9m2443js_pm_gpio {
+	int gpio;
+	unsigned long config;
+	int irq;
+	int state;
+	unsigned long flags;
+	irqreturn_t (* irq_handler)(int irq, void *gpio);
+};
 
 struct cc9m2443_pm_reg {
 	void __iomem *base;
@@ -75,7 +84,7 @@ static struct cc9m2443_pm_reg s3c2443_regs_syscon[] = {
 	CC9M2443_PM_ITEM_BASE(S3C2443_OSCSET),
 };
 
-/* If the offset is NULL, then it will not use it */
+/* If the passed register doesn't have a base register, the offset will be used */
 static void cc9m2443_pm_restore_regs(struct cc9m2443_pm_reg *regs, int count,
 				     void __iomem *offptr)
 {
@@ -93,6 +102,7 @@ static void cc9m2443_pm_restore_regs(struct cc9m2443_pm_reg *regs, int count,
 	}
 }
 
+/* If the passed register doesn't have a base register, the offset will be used */
 static void cc9m2443_pm_save_regs(struct cc9m2443_pm_reg *regs, int count,
 				  void __iomem *offptr)
 {
@@ -178,11 +188,6 @@ static int cc9m2443js_pm_suspend(struct sys_device *sd, pm_message_t state)
         return 0;
 }
 
-/*
- * @XXX: Probably this is not the best place for recovering the configuration of
- * the SSMC for the Ethernet-controller, but otherwise we don't have any other
- * option.
- */
 static int cc9m2443js_pm_resume(struct sys_device *sd)
 {
         __raw_writel(0x0, S3C2412_INFORM0);
@@ -194,6 +199,7 @@ static int cc9m2443js_pm_resume(struct sys_device *sd)
         return 0;
 }
 
+/* Internal system class/device for accessing to the PM-functions */
 static struct sysdev_class cc9m2443js_pm_sysclass = {
         .name           = "cc9m2443js-pm",
         .suspend        = cc9m2443js_pm_suspend,
@@ -204,53 +210,208 @@ static struct sys_device cc9m2443js_pm_sysdev = {
         .cls            = &cc9m2443js_pm_sysclass,
 };
 
-/* This function is used by the PM-support when using an external IRQ line */
-#if defined(CONFIG_MACH_CC9M2443JS_PM_EXTIRQ_GPIO)
-static irqreturn_t cc9m2443js_wakeup_irq(int irq, void *data)
+/* IRQ-handler for the GPIOs configured as wakeup-source */
+static irqreturn_t cc9m2443js_wakeup_irq(int irq, void *_gpio)
 {
-	return IRQ_HANDLED;
+	irqreturn_t retval;
+	struct cc9m2443js_pm_gpio *gpio;
+
+	gpio = (struct cc9m2443js_pm_gpio *)_gpio;
+	retval = IRQ_HANDLED;
+	if (gpio->irq_handler)
+		retval = gpio->irq_handler(irq, gpio);
+	
+	return retval;
 }
 
-static void __init cc9m2443js_pm_config(void)
+static int cc9m2443js_pm_gpio_probe(struct platform_device *pdev)
 {
-	unsigned int irqnr;
+	struct cc9m2443js_pm_gpio *gpio;
 	int retval;
 
-	irqnr = s3c2443_gpio_getirq(CONFIG_MACH_CC9M2443JS_PM_EXTIRQ_GPIO);
-
-	retval = request_irq(irqnr, cc9m2443js_wakeup_irq,
-			     IRQF_DISABLED | IRQF_TRIGGER_FALLING,
-			     "cc9m2443-wakeup", NULL);
-	if (retval) {
-		printk(KERN_ERR "[ ERROR ] Wakeup IRQ %u request failed\n", irqnr);
-		return;
+	gpio = pdev->dev.platform_data;	
+	gpio->irq = s3c2443_gpio_getirq(gpio->gpio);
+	if (gpio->irq < 0) {
+		printk(KERN_ERR "[ ERROR ] Invalid IRQ GPIO %i\n", gpio->gpio);
+		retval = -EINVAL;
+		goto exit_probe;
 	}
 
-	/* Now configure the GPIO as interrupt line */
-	s3c2443_gpio_cfgpin(CONFIG_MACH_CC9M2443JS_PM_EXTIRQ_GPIO,
-			    S3C2410_GPF0_EINT0);
+	retval = request_irq(gpio->irq, cc9m2443js_wakeup_irq,
+			     IRQF_DISABLED | gpio->flags, "cc9m2443-wio", gpio);
+	if (retval) {
+		printk(KERN_ERR "[ ERROR ] Requesting IRQ %i\n", gpio->irq);
+		gpio->irq = -EINVAL;
+		goto exit_probe;
+	}
 
-	/* And enable the IRQ as wakeup-source */
-	enable_irq_wake(irqnr);
-	return;
+	/*
+	 * Disable the IRQ first, we will activate it when we are going into
+	 * the sleep-mode
+	 */
+	disable_irq(gpio->irq);
+	s3c2443_gpio_cfgpin(gpio->gpio, gpio->config);
+	
+	/* Enable the wakeup support */
+	device_init_wakeup(&pdev->dev, 1);
+        device_set_wakeup_enable(&pdev->dev, 0);
+	
+	return 0;
+	
+exit_probe:
+	return retval;
 }
-#else
-static inline void cc9m2443js_pm_config(void)
+
+/* Free the requested resources (IRQ, etc.) */
+static int cc9m2443js_pm_gpio_remove(struct platform_device *pdev)
 {
-	/* Nothing to configure if no PM enabled */
+	struct cc9m2443js_pm_gpio *gpio;
+
+	gpio = pdev->dev.platform_data;
+
+	/* Free the requested GPIO and configure it as input */
+	free_irq(gpio->irq, gpio);
+
+	s3c2443_gpio_cfgpin(gpio->gpio, S3C2410_GPIO_INPUT);
+	
+	return 0;
 }
+
+/*
+ * Enable the IRQ when we are going into the sleep-mode and the GPIO is enabled
+ * as a wakeup-source
+ */
+static int cc9m2443js_pm_gpio_suspend(struct platform_device *pdev, pm_message_t state)
+{
+	struct cc9m2443js_pm_gpio *gpio;
+	int retval;
+
+	gpio = pdev->dev.platform_data;
+
+	if (device_may_wakeup(&pdev->dev)) {
+
+		if (gpio->irq <= 0) {
+			retval = -EINVAL;
+			goto exit_suspend;
+		}
+
+		enable_irq(gpio->irq);
+		
+		retval = enable_irq_wake(gpio->irq);
+		if (retval)
+			goto exit_suspend;
+	}
+
+	retval = 0;
+	
+exit_suspend:
+	return retval;
+}
+
+/*
+ * Disable the IRQ when resuming and the GPIO was enabled as a wakeup-source
+ */
+static int cc9m2443js_pm_gpio_resume(struct platform_device *pdev)
+{
+	struct cc9m2443js_pm_gpio *gpio;
+	int retval;
+
+	gpio = pdev->dev.platform_data;
+
+	retval = 0;
+	if (device_may_wakeup(&pdev->dev)) {
+
+		if (gpio->irq <= 0) {
+			retval = -ENODEV;
+			goto exit_resume;
+		}
+		
+		disable_irq_wake(gpio->irq);
+
+		disable_irq(gpio->irq);
+	}
+
+ exit_resume:
+	return retval;
+}
+
+/* Internal driver for the handling with wakeup GPIOs */
+static struct platform_driver cc9m2443js_pm_drv = {
+	.probe		= cc9m2443js_pm_gpio_probe,
+	.remove		= cc9m2443js_pm_gpio_remove,
+	.suspend	= cc9m2443js_pm_gpio_suspend,
+	.resume		= cc9m2443js_pm_gpio_resume,
+	.driver		= {
+		.owner		= THIS_MODULE,
+		.name		= "cc9m2443-wio"
+	}
+};
+
+/* Macro used for skipping the registering of the GPIO wakeup device */
+#define CC9M2443JS_PM_GPIO_DISABLED             (-1)
+
+/* Platform device for the wakeup GPIO 226 */
+static struct cc9m2443js_pm_gpio cc9m2443js_gpio226 = {
+#if defined(CONFIG_MACH_CC9M2443JS_PM_EXTIRQ_226)
+	.gpio           = 226,
+#else
+	.gpio           = CC9M2443JS_PM_GPIO_DISABLED,
 #endif
+	.config         = S3C2410_GPF0_EINT0,
+	.flags          = IRQF_TRIGGER_FALLING,
+};
+
+/* Platform device for the wakeup GPIO 232 */
+static struct cc9m2443js_pm_gpio cc9m2443js_gpio232 = {
+#if defined(CONFIG_MACH_CC9M2443JS_PM_EXTIRQ_232)
+	.gpio           = 232,
+#else
+	.gpio           = CC9M2443JS_PM_GPIO_DISABLED,
+#endif
+	.config         = S3C2410_GPF6_EINT6,
+	.flags          = IRQF_TRIGGER_FALLING,
+};
+
+/* Table of GPIOs that should be used as external wakeup */
+static struct platform_device cc9m2443js_pm_gpios[] = {
+	{
+		.name = "cc9m2443-wio",
+		.dev  = {
+			.platform_data = &cc9m2443js_gpio226,
+		}
+	},{
+		.name = "cc9m2443-wio",
+		.dev  = {
+			.platform_data = &cc9m2443js_gpio232,
+		}
+	}
+};
 
 /* This function generates the required sysclass for our platform */
 void __init cc9m2443js_pm_init(void)
 {
+	int cnt;
+	
 	sysdev_class_register(&cc9m2443js_pm_sysclass);
 	sysdev_register(&cc9m2443js_pm_sysdev);
 
+	/* Register our small driver for the wakeup IRQs */
+	platform_driver_register(&cc9m2443js_pm_drv);
 
-	cc9m2443js_pm_config();
+	/* Add the corresponding GPIOs */
+	for (cnt = 0; cnt < ARRAY_SIZE(cc9m2443js_pm_gpios); cnt++) {
+		struct cc9m2443js_pm_gpio *gpio;
+		struct platform_device *pdev;
 
-	
+		pdev = &cc9m2443js_pm_gpios[cnt];
+		gpio = pdev->dev.platform_data;
+		if (gpio->gpio == CC9M2443JS_PM_GPIO_DISABLED)
+			continue;
+
+		pdev->id = gpio->gpio;
+		platform_device_register(pdev);
+	}
+
 	/* This is the main function for the PM of the Samsung-platforms */
 	s3c2410_pm_init();
 }
