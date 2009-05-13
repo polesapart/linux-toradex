fdd359b0e934ec472918952b5397f7018ffc56fa
Pedro Perez de Heredia <pedro.perez@digi.com>
2009-05-13 20:08:15 +0200

    piper: removed calibration structures from airohaCalibration.h
    Signed-off-by: Pedro Perez de Heredia <pedro.perez@digi.com>

--- drivers/net/wireless/digiPiper/airohaCalibration.h
+++ drivers/net/wireless/digiPiper/airohaCalibration.h
@@ -10,53 +10,9 @@
  * the Free Software Foundation.
  */
 
-#ifndef airohaCalibration_h
-#define airohaCalibration_h
-
-#include "linux/types.h"
-
-#define WCD_MAGIC		        "WCALDATA"
-#define WCD_MAX_CAL_POINTS	    (8)
-#define WCD_CHANNELS_BG		    (14)
-#define WCD_CHANNELS_A		    (35)
-#define WCD_B_CURVE_INDEX       (0)
-#define WCD_G_CURVE_INDEX       (1)
-
-
-/*! \brief The wireless calibration information
- */
-typedef struct nv_wcd_header {
-	char	magic_string[8];	/* WCALDATA */
-	char	ver_major;		/* Major version in ascii */
-	char	ver_minor;		/* Minor version in ascii */
-	uint16_t hw_platform;		/* Hardware Platform used for calibration */
-	uint8_t	numcalpoints;		/* Number of points per curve */
-	uint8_t	padding[107];		/* Reserved for future use */
-	uint32_t wcd_len;		/* Total length of the data section */
-	uint32_t wcd_crc;		/* Data section crc32 */
-} nv_wcd_header_t;
-
-
-typedef struct wcd_point {
-	int16_t	 out_power;		/* Output Power */
-	uint16_t adc_val;		/* Measured ADC val */
-	uint8_t  power_index;		/* Airoha Power Index */
-	uint8_t  reserved[3];		/* For future use */
-} wcd_point_t;
-
-typedef struct wcd_curve {
-	uint8_t  max_power_index;	/* Airoha Max Power Index */
-	uint8_t  reserved[3];		/* Resered for future use */
-	wcd_point_t points[WCD_MAX_CAL_POINTS];	/* Calibration curve points */
-} wcd_curve_t;
-
-typedef struct wcd_data {
-	nv_wcd_header_t  header;
-	wcd_curve_t cal_curves_bg[WCD_CHANNELS_BG][2];
-    wcd_curve_t cal_curves_a[WCD_CHANNELS_A];
-} wcd_data_t;
+#ifndef AIROHA_CALIBRATION_H
+#define AIROHA_CALIBRATION_H
 
 void digiWifiInitCalibration(struct piper_priv *digi);
 
-
-#endif
+#endif /* AIROHA_CALIBRATION_H */
