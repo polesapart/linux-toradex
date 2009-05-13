28dac1f2762f284f9da6a72eb349e2713d1b901c
Pedro Perez de Heredia <pedro.perez@digi.com>
2009-05-13 20:13:35 +0200

    piper: removed some platform dependent stuff
    Signed-off-by: Pedro Perez de Heredia <pedro.perez@digi.com>

--- drivers/net/wireless/digiPiper/pipermain.h
+++ drivers/net/wireless/digiPiper/pipermain.h
@@ -6,9 +6,8 @@
 #include <linux/spinlock.h>
 #include <net/mac80211.h>
 #include <linux/i2c.h>
+#include <net/piper_pdata.h>
 
-// TODO:  Move this define into a header file shared with ns921x_devices.c
-#define PIPER_DRIVER_NAME   "ns9xxx-piper"
 #define DRV_VERS "0.1"
 
 
@@ -23,9 +22,6 @@
 
 typedef u64 u48;
 
-
-#define PIPER_RESET_GPIO    (92)
-#define PIPER_IRQ_GPIO      (104)
 
 /*
  * Set this #define to receive frames in the ISR.  This may improve 
