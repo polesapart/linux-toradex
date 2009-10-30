9d6c4d4aab4f14b22fdaf9c2ad866f41d9667f49
Hector Palacios <hector.palacios@digi.com>
2009-10-30 13:54:55 +0100

    s3c2443 irq: EXTINTx registers wrongly read (2nd)
    The practical tests in U-Boot have shown the following when writing
    and reading EXTINT registers:
    1. Writing seems to be ok
    2. For reading, the following happens:
      EXTINT0:
    	1. Read values are given in big endian (at nibble level):
    	   A read value of 0x00001234 is really 0x43210000.
    	2. The read value does not return the LSB:
    	   After writing 0xffffffff value read is 0x7fffffff.
      EXTINT1/2:
    	1. Read values are given in big endian (at nibble level)
    	and bits at nibble level suffer a 1 bit rotation to the left:
    	   After writing 0x12488822 value read is 0x44111842
    
    This patch does the necessary transformation to the nibbles so that
    EXTINT1/2 can be read properly, and thus correctly written when a
    bitfield must be written and is masked with the previously read value.
    
    irqs.h: add comments to better locate interrupts numbers.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- arch/arm/mach-s3c2410/include/mach/irqs.h
+++ arch/arm/mach-s3c2410/include/mach/irqs.h
@@ -65,17 +65,17 @@
 /* interrupts generated from the external interrupts sources */
 #define IRQ_EINT4      S3C2410_IRQ(32)	   /* 48 */
 #define IRQ_EINT5      S3C2410_IRQ(33)
-#define IRQ_EINT6      S3C2410_IRQ(34)
+#define IRQ_EINT6      S3C2410_IRQ(34)		/* 50 */
 #define IRQ_EINT7      S3C2410_IRQ(35)
 #define IRQ_EINT8      S3C2410_IRQ(36)
 #define IRQ_EINT9      S3C2410_IRQ(37)
 #define IRQ_EINT10     S3C2410_IRQ(38)
-#define IRQ_EINT11     S3C2410_IRQ(39)
+#define IRQ_EINT11     S3C2410_IRQ(39)		/* 55 */
 #define IRQ_EINT12     S3C2410_IRQ(40)
 #define IRQ_EINT13     S3C2410_IRQ(41)
 #define IRQ_EINT14     S3C2410_IRQ(42)
 #define IRQ_EINT15     S3C2410_IRQ(43)
-#define IRQ_EINT16     S3C2410_IRQ(44)
+#define IRQ_EINT16     S3C2410_IRQ(44)		/* 60 */
 #define IRQ_EINT17     S3C2410_IRQ(45)
 #define IRQ_EINT18     S3C2410_IRQ(46)
 #define IRQ_EINT19     S3C2410_IRQ(47)
--- arch/arm/plat-s3c24xx/irq.c
+++ arch/arm/plat-s3c24xx/irq.c
@@ -326,9 +326,22 @@ s3c_irqext_type(unsigned int irq, unsigned int type)
 
 	value = __raw_readl(extint_reg);
 
-        /* The below code is coming from the SMDK of Samsung */
+        /* According to Samsung's document 'S3C2443 guide to extra gpio'
+	 * several registers (including EXTINT0-2) must use a precompiled
+	 * function for read access. We don't have the original code but
+	 * we have seen the following behavior in U-Boot:
+	 *   EXTINT0:
+	 *	1. Read values are given in big endian (at nibble level):
+	 *	   a read value of 0x00001234 is really 0x43210000.
+	 *	2. The read value does not return the LSB:
+	 *	   after writing 0xffffffff value read is 0x7fffffff.
+	 *   EXTINT1/2:
+	 *	1. Read values are given in big endian (at nibble level)
+	 *	and bits at nibble level suffer a 1 bit rotation to the left:
+	 *	   after writing 0x12488822 value read is 0x44111842
+	 */
 #if defined(CONFIG_CPU_S3C2443)
-	t  =  (value  &  0x0000000f)  <<  28;
+	t  =   (value  &  0x0000000f)  <<  28;
 	t  |=  (value  &  0x000000f0)  <<  20;
 	t  |=  (value  &  0x00000f00)  <<  12;
 	t  |=  (value  &  0x0000f000)  <<  4;
@@ -337,8 +350,24 @@ s3c_irqext_type(unsigned int irq, unsigned int type)
 	t  |=  (value  &  0x0f000000)  >>  20;
 	t  |=  (value  &  0xf0000000)  >>  28;
 	value  =  t;
-#endif /* CONFIG_CPU_S3C2443 */
 
+	if (S3C24XX_EXTINT1 == extint_reg ||
+	    S3C24XX_EXTINT2 == extint_reg) {
+		/* Swap bits in nibbles */
+		unsigned char old_n, new_n;
+		int i;
+
+		t = 0;
+		for (i=0; i < 8; i++) {
+			old_n = (value & (0xf << (4*i))) >> (4*i);
+			new_n = ((old_n & 0xe) >> 1);
+			if (old_n & 0x1)
+				new_n |= 0x8;
+			t |= new_n << (4*i);
+		}
+		value = t;
+	}
+#endif /* CONFIG_CPU_S3C2443 */
 	value = (value & ~(7 << extint_offset)) | (newvalue << extint_offset);
 	__raw_writel(value, extint_reg);
 
