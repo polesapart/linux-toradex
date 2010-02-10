667505191359b2cb343560a65400a8ed8845fc6b
Pedro Perez de Heredia <pedro.perez@digi.com>
2010-02-10 15:06:58 +0100

    ccwmx51: add support for TFT LCD displays
    Signed-off-by: Pedro Perez de Heredia <pedro.perez@digi.com>

---
+++ arch/arm/mach-mx5/displays/LQ070Y3DG3B.h
@@ -0,0 +1,32 @@
+
+static void lcd_bl_enable_lq70(int enable)
+{
+	gpio_set_value(IOMUX_TO_GPIO(MX51_PIN_DI1_PIN11), enable);
+}
+
+static struct fb_videomode lq70y3dg3b = {
+	.name = "LQ070Y3DG3B",
+	.refresh = 60,
+	.xres = 800,
+	.yres = 480,
+	.pixclock = 29000,
+	.left_margin = 0,
+	.right_margin = 50,
+	.upper_margin = 25,
+	.lower_margin = 10,
+	.hsync_len = 128,
+	.vsync_len = 10,
+	.sync = FB_SYNC_CLK_LAT_FALL,
+	.vmode = FB_VMODE_NONINTERLACED,
+	.flag = 0,
+};
+
+#define LQ070Y3DG3B_DISPLAY				\
+{							\
+	.fb_pdata = {					\
+		.interface_pix_fmt = IPU_PIX_FMT_RGB24,	\
+		.mode_str = "LQ070Y3DG3B",		\
+		.mode = &lq70y3dg3b,			\
+	},						\
+	.bl_enable = &lcd_bl_enable_lq70,		\
+}
