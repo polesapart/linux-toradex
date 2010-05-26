c6b4922325734e995e007c39e7c734a81e45f538
Alejandro Gonzalez <alex.gonzalez@digi.com>
2010-05-26 16:50:42 +0200

    ccwmx51: Split machine specific data from platform device code.
    Signed-off-by: Alejandro Gonzalez <alex.gonzalez@digi.com>

---
+++ arch/arm/mach-mx5/devices_ccwmx51.h
@@ -0,0 +1,43 @@
+/*
+  * Copyright 2010 Digi International, Inc. All Rights Reserved.
+ */
+
+/*
+ * The code contained herein is licensed under the GNU General Public
+ * License. You may obtain a copy of the GNU General Public License
+ * Version 2 or later at the following locations:
+ *
+ * http://www.opensource.org/licenses/gpl-license.html
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+#ifndef DEVICES_CCWMX51_H_
+#define DEVICES_CCWMX51_H_
+
+extern struct flash_platform_data mxc_nand_data;
+extern struct smc911x_platdata ccwmx51_smsc9118;
+extern struct mxc_mmc_platform_data mmc1_data ;
+extern struct mxc_mmc_platform_data mmc3_data;
+extern struct resource mxcfb_resources[1];
+extern struct mxc_i2c_platform_data mxci2c_data;
+extern struct mxc_i2c_platform_data mxci2c_hs_data;
+extern struct mxc_spi_master mxcspi1_data ;
+extern struct mxc_srtc_platform_data srtc_data;
+extern struct mxc_ipu_config mxc_ipu_data;
+extern struct mxc_w1_config mxc_w1_data;
+extern struct mxc_spdif_platform_data mxc_spdif_data;
+extern struct tve_platform_data tve_data;
+extern struct mxc_dvfs_platform_data dvfs_core_data;
+extern struct mxc_dvfsper_data dvfs_per_data;
+extern struct platform_pwm_backlight_data mxc_pwm_backlight_data;
+extern struct mxc_audio_platform_data wm8753_data;
+extern struct mxc_fb_platform_data mx51_fb_data[];
+extern struct uio_info gpu2d_platform_data;
+extern struct ccwmx51_lcd_pdata * plcd_platform_data;
+extern int __init ccwmx51_init_i2c2(void);
+extern void ccwmx51_init_spidevices(void);
+extern int __init ccwmx51_init_fb(void);
+extern void __init ccwmx51_io_init(void);
+extern int __init ccwmx51_init_mc13892(void);
+
+#endif /* DEVICES_CCWMX51_H_ */
