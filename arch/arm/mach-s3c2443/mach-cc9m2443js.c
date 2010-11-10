6d21fd813c74c88378908db8c1d7fa9ef6949595
Hector Palacios <hector.palacios@digi.com>
2010-11-10 09:20:02 +0100

    pwm: enclose PWM device into ifdefs and init data
    PWM is only added by configuration switch.
    Number of channels is a needed parameter as it is passed to the
    pwm core registration function to allocate space for the channels.
    Platform data is initiated by an init function because array of
    channels is not statically dimensioned.
    
    Signed-off-by: Hector Palacios <hector.palacios@digi.com>

--- arch/arm/mach-s3c2443/mach-cc9m2443js.c
+++ arch/arm/mach-s3c2443/mach-cc9m2443js.c
@@ -65,6 +65,7 @@
 #include <mach/irqs.h>
 #include <mach/regs-irq.h>
 #include <plat/irq.h>
+#include <asm/plat-s3c24xx/pwm.h>
 
 #include <linux/mtd/mtd.h>
 #include <linux/mtd/nand.h>
@@ -585,7 +586,7 @@ static struct platform_device s3c2443_device_adc = {
 /* Pulse Width Modulation (PWM) timers */
 #define PWM_RESOURCE(_tmr, _irq)		\
 	{					\
-		.name   = "s3c24xx-pwm" _tmr,	\
+		.name   = "s3c24xx-pwm",	\
 		.start	= _irq,			\
 		.end	= _irq,			\
 		.flags	= IORESOURCE_IRQ,	\
@@ -599,12 +600,13 @@ static struct resource s3c2443_pwm_resources[] = {
 	/* timer4 has no output */
 };
 
-#define PWM_CHANNEL(_tmr, _gpio)			\
-	{						\
-		.timer		= _tmr,			\
-		.gpio		= _gpio,		\
+#define PWM_CHANNEL(_tmr, _gpio)	\
+	{				\
+		.timer	= _tmr,		\
+		.gpio	= _gpio,	\
 	}
 
+#ifdef CONFIG_S3C2443_PWM
 static struct s3c24xx_pwm_channel s3c2443_pwm_channels[] = {
 	PWM_CHANNEL(0, S3C2410_GPB0),
 	PWM_CHANNEL(1, S3C2410_GPB1),
@@ -612,10 +614,9 @@ static struct s3c24xx_pwm_channel s3c2443_pwm_channels[] = {
 	PWM_CHANNEL(3, S3C2410_GPB3),
 };
 
-static struct s3c24xx_pwm_pdata s3c2443_pwm_pdata = {
-	s3c2443_pwm_pdata.channels = s3c2443_pwm_channels;
-	s3c2443_pwm_pdata.number_channels = ARRAY_SIZE(s3c2443_pwm_channels);
-};
+/* This will be initialized in the init function to point to the
+ * channels array */
+static struct s3c24xx_pwm_pdata s3c2443_pwm_pdata;
 
 static struct platform_device s3c2443_device_pwm = {
 	.name		= "s3c24xx-pwm",
@@ -626,6 +627,15 @@ static struct platform_device s3c2443_device_pwm = {
 		.platform_data = &s3c2443_pwm_pdata,
 	}
 };
+
+static void __init s3c2443_pwm_init(void)
+{
+	/* Init platform data channels pointer to
+	 * the channels array */
+	s3c2443_pwm_pdata.number_channels = ARRAY_SIZE(s3c2443_pwm_channels);
+	s3c2443_pwm_pdata.channels = s3c2443_pwm_channels;
+}
+#endif
 
 /* Platform devices for the CC9M2443 */
 static struct platform_device *cc9m2443_devices[] __initdata = {
@@ -724,6 +734,11 @@ static void __init cc9m2443_machine_init(void)
 	s3c_device_nand.dev.platform_data = &cc9m2443_nand_info;
 
 	s3c_device_i2c.dev.platform_data = &cc9m2443_i2c_info;
+
+#ifdef CONFIG_S3C2443_PWM
+	/* Init PWM platform data with channels */
+	s3c2443_pwm_init();
+#endif
 
 	/* Enable the pullup for the write protect GPIO of the SD-port */
 	s3c2410_gpio_pullup(S3C2410_GPG9, 0x0);
--- include/asm-arm/plat-s3c24xx/pwm.h
+++ include/asm-arm/plat-s3c24xx/pwm.h
@@ -16,6 +16,9 @@
 
 #include <linux/clk.h>
 
+/* Maximal number of available channels */
+#define S3C24XX_PWM_CHANNEL_MAX                4
+
 /* This is the data for the PWM channels */
 struct s3c24xx_pwm_channel {
 	/* platform defined */
@@ -33,7 +36,7 @@ struct s3c24xx_pwm_channel {
 
 /* PWM data */
 struct s3c24xx_pwm_pdata {
-	unsigned int number_channels;
+	int number_channels;
  	struct s3c24xx_pwm_channel *channels;
 };
 
