00f6204e5ef6f2e9beba87557fdc5cc674bbdc66
Tony Lin <tony.lin@freescale.com>
2011-01-11 18:01:03 +0800

    ENGR00137979-2 [MX50]add performance monitor driver
    	enable performance monitor driver for system loading analysis
    
    Signed-off-by: Tony Lin <tony.lin@freescale.com>

--- arch/arm/mach-mx5/devices.h
+++ arch/arm/mach-mx5/devices.h
@@ -90,6 +90,8 @@ extern struct platform_device pm_device;
 extern struct platform_device fixed_volt_reg_device;
 extern struct platform_device mxc_zq_calib_device;
 extern struct platform_device mxc_asrc_device;
+extern struct platform_device mxc_perfmon;
+extern struct mxs_platform_perfmon_data mxc_perfmon_data;
 extern int z160_revision;
 extern struct platform_device lcd_pdev[];
 extern struct platform_device mxc_wm8753_device;
