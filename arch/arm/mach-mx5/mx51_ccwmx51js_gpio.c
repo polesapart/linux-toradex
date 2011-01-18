45977a874873c4285b601e60165869cdbbab0eb3
Alejandro Gonzalez <alex.gonzalez@digi.com>
2011-01-18 17:45:13 +0100

    [ccwmx51js]: add USB host support
    This commit adds support for the USB host interface H1.
    
    Signed-off-by: Alejandro Gonzalez <alex.gonzalez@digi.com>

--- arch/arm/mach-mx5/mx51_ccwmx51js_gpio.c
+++ arch/arm/mach-mx5/mx51_ccwmx51js_gpio.c
@@ -135,6 +135,76 @@ static struct mxc_iomux_pin_cfg __initdata ccwmx51_iomux_mmc_pins[] = {
 };
 #endif
 
+#if defined(CONFIG_USB_EHCI_ARC_H1) || defined(CONFIG_USB_EHCI_ARC_H1_MODULE)
+static struct mxc_iomux_pin_cfg __initdata ccwmx51_iomux_usbh1_pins[] = {
+	{	/* USBH1_STP */
+		MX51_PIN_USBH1_STP, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_PUE_KEEPER |
+		PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_CLK */
+		MX51_PIN_USBH1_CLK, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_PUE_KEEPER |
+		PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE | PAD_CTL_DDR_INPUT_CMOS),
+	},
+	{	/* USBH1_DIR */
+		MX51_PIN_USBH1_DIR, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_PUE_KEEPER |
+		PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE | PAD_CTL_DDR_INPUT_CMOS),
+	},
+	{	/* USBH1_NXT */
+		MX51_PIN_USBH1_NXT, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_PUE_KEEPER |
+		PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE | PAD_CTL_DDR_INPUT_CMOS),
+	},
+	{	/* USBH1_DATA0 */
+		MX51_PIN_USBH1_DATA0, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA1 */
+		MX51_PIN_USBH1_DATA1, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA2 */
+		MX51_PIN_USBH1_DATA2, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA3 */
+		MX51_PIN_USBH1_DATA3, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA4 */
+		MX51_PIN_USBH1_DATA4, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA5 */
+		MX51_PIN_USBH1_DATA5, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA6 */
+		MX51_PIN_USBH1_DATA6, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH1_DATA7 */
+		MX51_PIN_USBH1_DATA7, IOMUX_CONFIG_ALT0,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_100K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+	{	/* USBH PHY RESET */
+		MX51_PIN_DISPB2_SER_RS, IOMUX_CONFIG_GPIO,
+		(PAD_CTL_SRE_FAST | PAD_CTL_DRV_HIGH | PAD_CTL_47K_PU |
+		PAD_CTL_PUE_KEEPER | PAD_CTL_PKE_ENABLE | PAD_CTL_HYS_ENABLE),
+	},
+};
+#endif
+
 static struct mxc_iomux_pin_cfg __initdata ccwmx51_iomux_devices_pins[] = {
 	{	/* PMIC interrupt line */
 		MX51_PIN_GPIO1_5, IOMUX_CONFIG_GPIO | IOMUX_CONFIG_SION,
@@ -175,6 +245,25 @@ void __init ccwmx51_io_init(void)
 	/* PMIC interrupt line */
 	gpio_request(IOMUX_TO_GPIO(MX51_PIN_GPIO1_5), "gpio1_5");
 	gpio_direction_input(IOMUX_TO_GPIO(MX51_PIN_GPIO1_5));
+
+#if defined(CONFIG_USB_EHCI_ARC_H1) || defined(CONFIG_USB_EHCI_ARC_H1_MODULE)
+	for (i = 0; i < ARRAY_SIZE(ccwmx51_iomux_usbh1_pins); i++) {
+		mxc_request_iomux(ccwmx51_iomux_usbh1_pins[i].pin,
+				  ccwmx51_iomux_usbh1_pins[i].mux_mode);
+		if (ccwmx51_iomux_usbh1_pins[i].pad_cfg)
+			mxc_iomux_set_pad(ccwmx51_iomux_usbh1_pins[i].pin,
+					  ccwmx51_iomux_usbh1_pins[i].pad_cfg);
+		if (ccwmx51_iomux_usbh1_pins[i].in_select)
+			mxc_iomux_set_input(ccwmx51_iomux_usbh1_pins[i].in_select,
+					    ccwmx51_iomux_usbh1_pins[i].in_mode);
+	}
+
+	/* USB PHY/HUB reset*/
+	gpio_request(IOMUX_TO_GPIO(MX51_PIN_DISPB2_SER_RS), "gpio3_8");
+	gpio_direction_output(IOMUX_TO_GPIO(MX51_PIN_DISPB2_SER_RS), 0);
+	msleep(1);
+	gpio_set_value(IOMUX_TO_GPIO(MX51_PIN_DISPB2_SER_RS), 1);
+#endif
 
 #if defined(CONFIG_MMC_IMX_ESDHCI) || defined(CONFIG_MMC_IMX_ESDHCI_MODULE)
 	for (i = 0; i < ARRAY_SIZE(ccwmx51_iomux_mmc_pins); i++) {
