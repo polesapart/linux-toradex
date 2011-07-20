1cef4db2b396dcaec959034dfdf00f2ff35e9390
Ranjani Vaidyanathan <ra5478@freescale.com>
2011-07-20 14:40:09 -0500

    ENGR00153567: MX5x - Fix race condition in clock code
    There is possible race condition between enabling/disabling clocks
    and the corresponding calls to change the bus frequencies.
    Fix by ensuring that the flags used to change the bus frequencies
    are set in a atomic context and before the bus frequency is
    actually changed.
    
    Signed-off-by: Ranjani Vaidyanathan <ra5478@freescale.com>

--- arch/arm/mach-mx5/clock.c
+++ arch/arm/mach-mx5/clock.c
@@ -4799,6 +4799,10 @@ int __init mx51_clocks_init(unsigned long ckil, unsigned long osc, unsigned long
 	 */
 	clk_enable(&cpu_clk);
 #endif
+
+	lp_med_freq = 0;
+	lp_high_freq = 0;
+
 	return 0;
 }
 
@@ -5089,6 +5093,10 @@ int __init mx53_clocks_init(unsigned long ckil, unsigned long osc, unsigned long
 			1190000));
 	base = ioremap(MX53_BASE_ADDR(GPT1_BASE_ADDR), SZ_4K);
 	mxc_timer_init(&gpt_clk[0], base, MXC_INT_GPT);
+
+	lp_med_freq = 0;
+	lp_high_freq = 0;
+
 	return 0;
 }
 
