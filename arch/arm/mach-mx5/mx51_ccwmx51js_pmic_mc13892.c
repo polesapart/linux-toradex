f93dfa974ce4aae72ee731baf3f301742df4b87f
Alex Gonzalez <alex.gonzalez@digi.com>
2011-11-10 10:21:26 +0100

    ccwmx51: Do not enable the PMIC coin cell charger
    The EAK and JSK do not have a rechargable battery but a primary cell.
    
    Signed-off-by: Alex Gonzalez <alex.gonzalez@digi.com>

--- arch/arm/mach-mx5/mx51_ccwmx51js_pmic_mc13892.c
+++ arch/arm/mach-mx5/mx51_ccwmx51js_pmic_mc13892.c
@@ -387,11 +387,6 @@ static int mc13892_regulator_init(struct mc13892 *mc13892)
 		pmic_write_reg(REG_SW_5, value, 0xffffff);
 	}
 
-	/* Enable coin cell charger */
-	value = BITFVAL(COINCHEN, 1) | BITFVAL(VCOIN, VCOIN_3_0V);
-	register_mask = BITFMASK(COINCHEN) | BITFMASK(VCOIN);
-	pmic_write_reg(REG_POWER_CTL0, value, register_mask);
-
 #if defined(CONFIG_RTC_DRV_MXC_V2) || defined(CONFIG_RTC_DRV_MXC_V2_MODULE)
 	value = BITFVAL(DRM, 1);
 	register_mask = BITFMASK(DRM);
