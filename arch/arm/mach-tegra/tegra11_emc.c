e072a123bc26ba99eb6736b16a2f28a92fe7aa5e
Kenneth Kwak <kkwak@nvidia.com>
2013-06-26 11:23:19 +0900

    ARM: tegra: clock: change iso efficiency when vde is on.
    Add vde.emc node. Change the display efficiency rate to 45% once
    dc.emc and vde.emc are both enabled.
    
    Bug 1266369
    
    Change-Id: If66f10d31c198bddcc18090d28639cd3beea86de
    Signed-off-by: Kevin Huang <kevinh@nvidia.com>
    Signed-off-by: Daniel Solomon <daniels@nvidia.com>
    Reviewed-on: http://git-master/r/232804
    (cherry picked from commit 51b46602daf09d8103f23291d3dcce078a4c1b37)
    
    Signed-off-by: Kenneth Kwak <kkwak@nvidia.com>
    Change-Id: I86c3892ad0014937bba55b7265adfb1cc3037d6a
    Reviewed-on: http://git-master/r/242226
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Peter Kim <pekim@nvidia.com>

--- arch/arm/mach-tegra/tegra11_emc.c
+++ arch/arm/mach-tegra/tegra11_emc.c
@@ -67,6 +67,7 @@ u8 tegra_emc_bw_efficiency = 80;
 static struct emc_iso_usage tegra11_ddr3_emc_iso_usage[] = {
 	{ BIT(EMC_USER_DC),			50},
 	{ BIT(EMC_USER_DC) | BIT(EMC_USER_VI),	35},
+	{ BIT(EMC_USER_DC) | BIT(EMC_USER_VDE),	45},
 };
 
 static struct emc_iso_usage tegra11_lpddr3_emc_iso_usage[] = {
@@ -85,6 +86,10 @@ static struct emc_iso_usage tegra11_lpddr3_emc_iso_usage[] = {
 	{
 		BIT(EMC_USER_DC) | BIT(EMC_USER_3D),
 		50, iso_share_calc_t114_lpddr3_default
+	},
+	{
+		BIT(EMC_USER_DC) | BIT(EMC_USER_VDE),
+		45, iso_share_calc_t114_lpddr3_default
 	},
 };
 
