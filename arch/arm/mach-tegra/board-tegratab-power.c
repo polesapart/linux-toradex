93ee60b0fab918437c97de8ce9c270540de19114
Jinyoung Park <jinyoungp@nvidia.com>
2013-07-17 16:19:58 +0900

    ARM: tegra: tegratab: Enable extended delay on TPS65913 GPADC
    Enable extended delay on TPS65913 GPADC in order to help system
    settles to loaded situation.
    
    Bug 1287901
    
    Change-Id: I4ebee3289c0c020c9a7e18de16adf1261c69eebe
    Signed-off-by: Jinyoung Park <jinyoungp@nvidia.com>
    Reviewed-on: http://git-master/r/250157
    Reviewed-by: Gabby Lee <galee@nvidia.com>
    Tested-by: Gabby Lee <galee@nvidia.com>

--- arch/arm/mach-tegra/board-tegratab-power.c
+++ arch/arm/mach-tegra/board-tegratab-power.c
@@ -411,10 +411,10 @@ static struct iio_map palmas_adc_iio_maps[] = {
 static struct palmas_gpadc_platform_data palmas_adc_pdata = {
 	/* If ch3_dual_current is true, it will measure ch3 input signal with
 	 * ch3_current and the next current of ch3_current.
-	 * So this system will use 10uA and 400uA for ch3 measurement. */
+	 * So this system will use 400uA and 800uA for ch3 measurement. */
 	.ch3_current_uA = 400,	/* 0uA, 10uA, 400uA, 800uA */
 	.ch3_dual_current = true,
-	.extended_delay = false,
+	.extended_delay = true,
 	.iio_maps = palmas_adc_iio_maps,
 };
 
